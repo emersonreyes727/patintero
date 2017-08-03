9043f061d61c51ccd78a605219fbe90d0673e50b
emerson reyes <emersonreyes727@gmail.com>
2017-08-02 22:03:38 -0700

    Create game manager and guard walk script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	//
+	private bool gameStart = false;
+	private bool gameRestart = false;
+
+	//
+	private bool levelOneComplete = false;
+	private bool levelTwoComplete = false;
+	private bool levelThreeComplete = false;
+	private bool levelFourComplete = false;
+	private bool levelFiveComplete = false;
+	private bool levelSixComplete = false;
+	private bool levelSevenComplete = false;
+	private bool levelEightComplete = false;
+	private bool levelNineComplete = false;
+	private bool levelTenComplete = false;
+
+	//
+	public bool GameStart {
+		get { return gameStart; }
+	}
+
+	public bool GameRestart {
+		get { return gameRestart; }
+	}
+
+	//
+	public bool LevelOneComplete {
+		get { return levelOneComplete; }
+	}
+
+	public bool LevelTwoComplete {
+		get { return levelTwoComplete; }
+	}
+
+	public bool LevelThreeComplete {
+		get { return levelThreeComplete; }
+	}
+
+	public bool LevelFourComplete {
+		get { return levelFourComplete; }
+	}
+
+	public bool LevelFiveComplete {
+		get { return levelFiveComplete; }
+	}
+
+	public bool LevelSixComplete {
+		get { return levelSixComplete; }
+	}
+
+	public bool LevelSevenComplete {
+		get { return levelSevenComplete; }
+	}
+
+	public bool LevelEightComplete {
+		get { return levelEightComplete; }
+	}
+
+	public bool LevelNineComplete {
+		get { return levelNineComplete; }
+	}
+
+	public bool LevelTenComplete {
+		get { return levelTenComplete; }
+	}
+
+	//
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+	}
+
+	//
+	public void PlayerStartGame () {
+		gameStart = true;
+	}
+
+	public void PlayerReStartGame () {
+		gameRestart = true;
+	}
+}
---
+++ Assets/Scripts/GuardWalk.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardWalk : MonoBehaviour {
+
+	[SerializeField] private Transform target;
+	private NavMeshAgent nav;
+
+	//
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+	}
+	
+	// 
+	void Update () {
+		nav.SetDestination (target.position);
+
+		if (GameManager.instance.GameStart == true) {
+			// random speed from .5 to 1, for levels 1 and 2
+			float randomSpeed = Random.Range (0.5f, 1f);
+			Debug.Log (randomSpeed);
+			nav.speed = randomSpeed;
+		}
+
+		if (GameManager.instance.LevelTwoComplete == true) {
+			// random speed from .8 to 1.5, for levels 3 and 4
+			float randomSpeed = Random.Range (.8f, 1.5f);
+			Debug.Log (randomSpeed);
+			nav.speed = randomSpeed;
+		}
+
+		if (GameManager.instance.LevelFourComplete == true) {
+			// random speed from 1.3 to 2, for levels 5 and 6
+			float randomSpeed = Random.Range (1.3f, 2f);
+			Debug.Log (randomSpeed);
+			nav.speed = randomSpeed;
+		}
+
+		if (GameManager.instance.LevelSixComplete == true) {
+			// random speed from 1.8 to 2.5, for levels 7 and 8
+			float randomSpeed = Random.Range (1.8f, 2.5f);
+			Debug.Log (randomSpeed);
+			nav.speed = randomSpeed;
+		}
+
+		if (GameManager.instance.LevelEightComplete == true) {
+			// random speed from 2.2 to 2.7, for levels 9 and 10
+			float randomSpeed = Random.Range (2.2f, 2.7f);
+			Debug.Log (randomSpeed);
+			nav.speed = randomSpeed;
+		}
+	}
+}
